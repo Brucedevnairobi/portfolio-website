252874c79b2a0112515c4bd497cf8e6550f0143f
Brucedevnairobi <bruce14simiyu@gmail.com>
2024-07-22 11:56:21 +0300

    theme alignment

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -21,7 +21,7 @@ const NavBar = () => {
               </li>
             ))}
           </ul>
-          <section className="ml-auto md:ml-12">
+          <section className="ml-auto md:ml-4">
             <Theme />
           </section>
         </div>
